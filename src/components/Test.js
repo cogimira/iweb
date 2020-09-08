32e76111d4693906d35b46ae8e4dc98fa7671d59
wanghuaiyao <wanghuaiyao@hetao101.com>
2020-09-08 15:31:23 +0800

    first

---
+++ src/components/Test.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+import Test2 from './Test2.js';
+export default {
+    render(h) {
+      return <div>
+          sdsdsdsdsd
+          <Test2/>
+      </div> // no need to register Todo via components option
+    },
+  }
---
+++ src/components/Test2.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+export default {
+    render(h) {
+      return <div>
+          this is test2
+      </div> // no need to register Todo via components option
+    },
+  }
